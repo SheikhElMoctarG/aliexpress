3459b5361592660780592b0935ca6a7ac303f9e4
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-02-02 19:41:31 +0000

    update

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -11,7 +11,22 @@
                 <Icon name="ic:sharp-install-mobile" size="17"/>
                 App
             </li>
+            <li @mouseenter="isAccountMenu = true" @mouseleave="isAccountMenu = false"
+                class="relative flex items-center px-2.5 hover:text-[#ff4646] h-full cursor-pointer"
+                :class="isAccountMenu ? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgba(0,0,0,0.3)]' : 'border border-[#fafafa]'">
+                <Icon name="ph:user-thin" size="17"/>
+                Account 
+                <Icon name="mdi:chevron-down" size="15" class="ml-5"/>
+                <div id="AccountMenu" v-if="isAccountMenu" class="absolute bg-white w-[220px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b">
+                    <div v-if="true">
+                        <div class="font-semibold text-[15px] my-4 px-3">Welcome to AliExpress!</div>
+                    </div>
+                </div>
+            </li>
             </ul>
         </div>
     </div>
 </template>
+<script setup>
+let isAccountMenu = ref(false);
+</script>
