d5881c481b24d9f60551e8eea89ed3976e0ee07c
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-02-03 09:04:23 +0000

    first account

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -20,6 +20,11 @@
                 <div id="AccountMenu" v-if="isAccountMenu" class="absolute bg-white w-[220px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b">
                     <div v-if="true">
                         <div class="font-semibold text-[15px] my-4 px-3">Welcome to AliExpress!</div>
+                        <div class="flex items-center gap-1 px-3 mb-3">
+                            <NuxtLink to="/auth" class="bg-[#ff4646] text-center rounded-sm text-white w-full py-2">
+                                Login / Register
+                            </NuxtLink>
+                        </div>
                     </div>
                 </div>
             </li>
