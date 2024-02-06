8a6be770a66c5df09c4f2862b508f5d8dbcd240a
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-02-06 15:32:06 +0000

    add menu overlay componot this is first work for me now

--- pages/auth.vue
+++ pages/auth.vue
@@ -52,11 +52,11 @@
 <script setup>
 const client = useSupabaseClient();
 const user = useSupabaseUser();
-watchEffect(()=> {
-    if(user.value){
-        return navigateTo('/')
-    }
-});
+// watchEffect(()=> {
+//     if(user.value){
+//         return navigateTo('/')
+//     }
+// });
 const login = async (prov)=> {
     const {data, error } = await client.auth.signInWithOAuth({
         provider: prov
--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,7 @@
       </div> 
     </MainLayout>
     
+    
 </template>
 
 <script setup>
