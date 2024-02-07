c08439aea6d4aabe273b7fd54c184df9103e7e33
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-02-07 16:57:56 +0000

    test netlify

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
   <MenuOverlay :class="[
     {'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isMenuOverlay },
     {'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isMenuOverlay },
-  ]"/>
+  ]"/> 
 </template>
 <script setup>
 import { useUserStore } from '~/stores/user';
